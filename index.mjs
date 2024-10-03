8b8af46188d480591df73a4c6ff6bce63b754c83
Emile-Webdev <emile.webdev@hotmail.com>
2024-10-03 13:58:11 +0200

    Completed

--- index.mjs
+++ index.mjs
@@ -1,11 +1,22 @@
-class HashMap {
-    constructor() {
+class Node {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        this.nextNode = null;
+    }
+}
 
+class HashMap {
+    constructor(capacity = 16, loadFactor = 0.75) {
+        this.capacity = capacity;
+        this.loadFactor = loadFactor;
+        this.size = 0;
+        this.bucketArray = new Array(this.capacity);
     }
 
     // Throw an error if we try to access an out of bound index
     checkIndex(index){
-        if(index < 0 || index >= this.buckets.length){
+        if (index < 0 || index >= this.bucketArray.length) {
             throw new Error("Trying to access index out of bound")
         }
     }
@@ -23,47 +34,144 @@ class HashMap {
 
     // Takes two arguments, a key and a value that is assigned to this key
     set(key, value) {
-        
+        let bucketIndex = this.hash(key) % this.capacity;
+        this.checkIndex(bucketIndex);
+
+        if (!this.bucketArray[bucketIndex]) {
+            this.bucketArray[bucketIndex] = new Node(key, value);
+            this.size++;
+        } else {
+            let node = this.bucketArray[bucketIndex];
+
+            while(node) {
+                if (node.key === key) {
+                    node.value = value;
+                    return;
+                } else if (!node.nextNode) {
+                    node.nextNode = new Node(key, value);
+                    this.size++;
+                    break;
+                }
+                node = node.nextNode;
+            }
+        }
     }
 
     // Takes one argument as a key and returns the value that is assigned to this key
     get(key) {
-        
+        let bucketIndex = this.hash(key) % this.capacity;
+        this.checkIndex(bucketIndex);
+
+        if (this.bucketArray[bucketIndex]) {
+            let node = this.bucketArray[bucketIndex];
+
+            while(node) {
+                if(node.key === key) {
+                    return node.value;
+                } 
+                node = node.nextNode;
+            }
+        }
+        return null;
     }
 
     // Takes a key as an argument and returns true or false based on whether or not the key is in the hash map
     has(key) {
+        let bucketIndex = this.hash(key) % this.capacity;
+        this.checkIndex(bucketIndex);
 
+        if (this.bucketArray[bucketIndex]) {
+            let node = this.bucketArray[bucketIndex];
+
+            while(node) {
+                if (node.key === key) {
+                    return true;
+                }
+                node = node.nextNode;
+            }
+        }
+        return false;
     }
 
     // If the given key is in the hash map, remove the entry with that key and return true. If the key isn’t in the hash map, return false
     remove(key) {
+        let bucketIndex = this.hash(key) % this.capacity;
+        this.checkIndex(bucketIndex);
 
+        if (this.bucketArray[bucketIndex]) {
+            if (this.bucketArray[bucketIndex].key === key) {
+                this.bucketArray[bucketIndex] = this.bucketArray[bucketIndex].nextNode;
+                this.size--;
+            } else {
+                let node = this.bucketArray[bucketIndex];
+                let previousNode = null;
+
+                while(node) {
+                    if (node.key === key) {
+                        if (previousNode) {
+                            previousNode.nextNode = node.nextNode;
+                        }
+                        this.size--;
+                        break;
+                    }
+                    previousNode = node;
+                    node = node.nextNode;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 
     // Returns the number of stored keys in the hash map
     length() {
-
+        return this.size;
     }
 
     // Removes all entries in the hash map
     clear() {
-
+        this.capacity = 16;
+        this.bucketArray = new Array(this.capacity);
+        this. size = 0;
     }
 
     // Returns an array containing all the keys inside the hash map
     keys() {
+        const keysArray = [];
 
+        this.bucketArray.forEach(node => {
+            while(node) {
+                keysArray.push(node.key);
+                node = node.nextNode;
+            }
+        })
+        return keysArray;
     }
 
     // Returns an array containing all the values
     values() {
+        const valuesArray = [];
 
+        this.bucketArray.forEach(node => {
+            while(node) {
+                valuesArray.push(node.value);
+                node = node.nextNode;
+            }
+        })
+        return valuesArray;
     }
 
     // Returns an array that contains each key, value pair
     entries() {
+        const keysValuesArray = [];
 
+        this.bucketArray.forEach(node => {
+            while(node) {
+                keysValuesArray.push([node.key, node.value]);
+                node = node.nextNode;
+            }
+        })
+        return keysValuesArray;
     }
 }
 
--- main.mjs
+++ main.mjs
@@ -1,6 +1,8 @@
 import { HashMap } from "./index.mjs";
 
 const test = new HashMap();
+
+// After adding hash map with the data below, hash map’s actual capacity should now be at 0.75 (full capacity).
 test.set('apple', 'red');
 test.set('banana', 'yellow');
 test.set('carrot', 'orange');
@@ -13,3 +15,35 @@ test.set('ice cream', 'white');
 test.set('jacket', 'blue');
 test.set('kite', 'pink');
 test.set('lion', 'golden');
+console.log(test.get('grape'));
+
+// Test overwriting a few nodes using set(key, value)
+test.set('jacket', 'leather');
+test.set('kite', 'large');
+test.set('lion', 'hungry');
+console.log(test.get('jacket'));
+console.log(test.get('kite'));
+console.log(test.get('lion'));
+
+// Test adding new node, hash map will exceed current load factor, expanding buckets and growing hash map
+test.set('moon', 'silver');
+console.log(`New Capacity: ${test.capacity}`);
+
+// With new hash map, test overwriting a few nodes using set(key, value). 
+test.set('moon', 'bright');
+test.set('dog', 'bark');
+console.log(test.get('dog'));
+console.log(test.get('moon'));
+
+// Test the other methods of hash maps such as get(key), has(key), remove(key), length(), clear(), keys(), values(), and entries() to check if they are still working as expected after expanding hash map.
+console.log(test.get('apple'));       // get(key)
+console.log(test.has('banana'));      // has(key)
+console.log(test.has('melon')); 
+test.remove('elephant');              // remove(key)
+console.log(test.get('elephant'));   
+console.log(test.length());           // length()
+test.clear();                         // clear()
+console.log(test.length()); 
+console.log(test.keys());             // keys()
+console.log(test.values());           // values()
+console.log(test.entries());          // entries()
